995c85d253db2ffe078b1d4188cb34389e01563b
BrahmaAztra <88469072+BrahmaAztra@users.noreply.github.com>
2021-08-05 08:08:47 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+/*******************************************************************************************************
+ 
+ I created this script in order to show basic CRUDE operations to the HILIFE enterprises
+ *******************************************************************/
+
+ require_once 'header.php';
+?>
+<div class="container">
+	<div class="jumbotron">
+	<h1>Basic CRUD in PHP</h1>
+	<p>Buenas adios mi amigos.</p>
+</div>
+</div>
+<?php 
+
+ require_once 'footer.php';
